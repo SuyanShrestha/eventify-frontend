4f4f934ef383bbbf2427525213efa343318974bf
SuyanShrestha <sh.suyan16@gmail.com>
2025-02-19 22:05:21 +0545

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-blue-600 text-white">
+      <h1 className="text-4xl font-bold">Hello, Tailwind with Vite!</h1>
+    </div>
+  );
+};
+
+export default App;
