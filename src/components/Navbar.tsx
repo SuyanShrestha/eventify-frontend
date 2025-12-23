1e5c8f8a85280ed45342810e9ccfd13554b5d9c0
SuyanShrestha <sh.suyan16@gmail.com>
2025-12-23 22:33:45 +0545

    chore: only show notifications bell when user is logged in

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -135,9 +135,9 @@ const Navbar: React.FC = () => {
             </div>
           </div>
           <div className="flex gap-4 ml-4 h-full items-center justify-center">
-            <button className="cursor-pointer" onClick={openNotificationModal}>
+            {isLoggedIn && <button className="cursor-pointer" onClick={openNotificationModal}>
               <Bell className="h-6 w-6 text-accent-500" />
-            </button>
+            </button>}
             <button className="cursor-pointer" onClick={openAuthModal}>
               <User className="h-6 w-6 text-accent-500" />
             </button>
